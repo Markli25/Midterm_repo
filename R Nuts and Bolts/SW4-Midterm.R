ff931caadd594ff93b3178a6815eca66d2dd6c0d
markli25 <markchristianli25@gmail.com>
2018-08-23 15:54:30 +0800

    SW4-Midterm (Mark Christian C. Li) August 23,2018

---
+++ R Nuts and Bolts/SW4-Midterm.R
@@ -0,0 +1,21 @@
+
+
+#readimagethrulocal
+install.packages("imager")
+library(imager)
+file <- system.file('extdata/index.jpg',package='imager')
+im <- load.image(file)
+plot(im)
+
+#readimagethrulocalresized
+library(imager)
+file <- system.file('extdata/index.jpg',package='imager')
+im <- load.image(file)
+im <- resize(im, 250, 250)
+plot(im,main="RESIZED")
+
+#readimagethruweb
+install.packages("magick")
+library(magick)
+tiger <- image_read('https://lh3.googleusercontent.com/-7C7aA4HEtoM/VT7Xh7f0RWI/AAAAAAAAARI/HVLyAAUerwA/w530-h298-n/522622.jpg')
+image_flop(tiger)
